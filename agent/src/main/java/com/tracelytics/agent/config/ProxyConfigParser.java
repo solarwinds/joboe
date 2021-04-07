d8fb2707d15b343736d2250f855a76b5ec750aaf
patsonluk <patson@solarwinds.cloud>
2021-04-07 15:37:37 -0700

    [config] Avoid printing the proxy config string on error

--- agent/src/main/java/com/tracelytics/agent/config/ProxyConfigParser.java
+++ agent/src/main/java/com/tracelytics/agent/config/ProxyConfigParser.java
@@ -20,23 +20,23 @@ public class ProxyConfigParser implements ConfigParser<String, ProxyConfig> {
         try {
             proxyUrl = new URL(proxyString);
         } catch (MalformedURLException e) {
-            throw new InvalidConfigException("Failed to parse proxy string value [" + proxyString + "] Error message is [" + e.getMessage() + "]", e);
+            throw new InvalidConfigException("Failed to parse proxy string value. The config value should either be http://<proxyHost>:<proxyPort> for a proxy server that does not require authentication, or http://<username>:<password>@<proxyHost>:<proxyPort> for a proxy server that requires Basic authentication. Error message is [" + e.getMessage() + "]", e);
         }
 
         //only support http:// for now
         if (!"http".equals(proxyUrl.getProtocol())) {
-            throw new InvalidConfigException("Failed to parse proxy string value [" + proxyString + "]. Unsupported protocol [" + proxyUrl.getProtocol() + "]. Currently support only proxying via a HTTP proxy server");
+            throw new InvalidConfigException("Failed to parse proxy string value. Unsupported protocol [" + proxyUrl.getProtocol() + "]. Currently support only proxying via a HTTP proxy server");
         }
 
         if (proxyUrl.getPort() == -1) {
-            throw new InvalidConfigException("Failed to parse proxy string value [" + proxyString + "]. Missing/Invalid port number.");
+            throw new InvalidConfigException("Failed to parse proxy string value. Missing/Invalid port number.");
         }
 
         String userInfo = proxyUrl.getUserInfo();
         if (userInfo != null) {
             String[] tokens = userInfo.split(":", 2);
             if (tokens.length != 2) {
-                throw new InvalidConfigException("Failed to parse proxy config's user/password [" + userInfo + "], it should be in username:password format");
+                throw new InvalidConfigException("Failed to parse proxy config's user/password, it should be in username:password format");
             }
 
             return new ProxyConfig(proxyUrl.getHost(), proxyUrl.getPort(), tokens[0], tokens[1]);
