10e3f52293dac175a3dcb53f771bccc077784c86
patsonluk <patson@solarwinds.cloud>
2021-05-14 12:18:40 -0700

    [servlet] No distributed tracing for server push. Mark span as async

--- instrumentation/src/main/java/com/tracelytics/instrumentation/http/ServletPushBuilderInstrumentation.java
+++ instrumentation/src/main/java/com/tracelytics/instrumentation/http/ServletPushBuilderInstrumentation.java
@@ -8,6 +8,7 @@ import com.tracelytics.instrumentation.MethodMatcher;
 import com.tracelytics.joboe.Context;
 import com.tracelytics.joboe.span.impl.Scope;
 import com.tracelytics.joboe.span.impl.ScopeManager;
+import com.tracelytics.joboe.span.impl.Span;
 import com.tracelytics.joboe.span.impl.Tracer;
 
 import java.util.Arrays;
@@ -16,13 +17,14 @@ import java.util.List;
 /**
  * Instruments `javax.servlet.http.PushBuilder` for the Server Push operation from Servlet 4.0.
  *
- * A span is added when `push` is invoked https://javaee.github.io/javaee-spec/javadocs/javax/servlet/http/PushBuilder.html#push--
+ * A async span is added when `push` is invoked https://javaee.github.io/javaee-spec/javadocs/javax/servlet/http/PushBuilder.html#push--
  *
  * Take note that the push call itself is non-blocking and should be quite short, we are reporting it to keep track of the "pushed" path,
  *
- * such that it can be matched with the nested async span - the processing done for the "pushed" path, which is just another regular servlet triggered by the app container internally.
+ * such that it can be matched with the triggered server push pan - the processing done for the "pushed" path, which is just another regular servlet triggered by the app container internally.
  *
- * Take note that in order to link the nested async span, we will need to invoke `PushBuilder#setHeader` to set the x-trace header when the app container forwards the request.
+ * Take note that we currently do NOT link the trigger server push span, (possible with `PushBuilder#setHeader` to set the x-trace header when the app container forwards the request),
+ * due to UI rendering issue as documented in https://github.com/librato/joboe/pull/1477
  *
  * @author pluk
  */
@@ -40,20 +42,18 @@ public class ServletPushBuilderInstrumentation extends ClassInstrumentation {
         throws Exception {
         for (CtMethod pushMethod : findMatchingMethods(cc, methodMatchers).keySet()) {
             //has to insert BEFORE the complete() call, otherwise some newer servlet implementation might throw except for calling getRequest() after complete()
-            insertBefore(pushMethod, "setHeader(\"" + HeaderConstants.XTRACE_HEADER + "\", " + CLASS_NAME +  ".pushEntry(this.getPath()));", false);
+            //insertBefore(pushMethod, "setHeader(\"" + HeaderConstants.XTRACE_HEADER + "\", " + CLASS_NAME +  ".pushEntry(this.getPath()));", false);
+            insertBefore(pushMethod, CLASS_NAME +  ".pushEntry(this.getPath());");
             insertAfter(pushMethod, CLASS_NAME + ".pushExit();", true);
         }
         
         return true;
     }
     
-    public static String pushEntry(String path) {
-        if (Context.getMetadata().isSampled()) {
-            Tracer.SpanBuilder spanBuilder = buildTraceEventSpan(SPAN_NAME);
-            spanBuilder.withTag("PushPath", path);
-            spanBuilder.startActive();
-        }
-        return Context.getMetadata().toHexString();
+    public static void pushEntry(String path) {
+        Tracer.SpanBuilder spanBuilder = buildTraceEventSpan(SPAN_NAME);
+        spanBuilder.withTag("PushPath", path).withSpanProperty(Span.SpanProperty.IS_ASYNC, true);
+        spanBuilder.startActive();
     }
 
     public static void pushExit() {
