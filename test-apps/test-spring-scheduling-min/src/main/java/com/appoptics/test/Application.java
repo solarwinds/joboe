b45c2df7d3fcc0649b00251365e46b5c289d3a23
patsonluk <patsonluk@users.noreply.github.com>
2021-06-03 10:03:10 -0700

    Update Application.java

--- test-apps/test-spring-scheduling-min/src/main/java/com/appoptics/test/Application.java
+++ test-apps/test-spring-scheduling-min/src/main/java/com/appoptics/test/Application.java
@@ -7,7 +7,7 @@ public class Application {
     public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Thread.sleep(30000);
+        Thread.sleep(3000000); //run the test for 3000 sec (50 mins)
         context.close();
     }
 }
