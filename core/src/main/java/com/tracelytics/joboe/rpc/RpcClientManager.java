3632b529f9e73f42971d326a0e79b1e35f3109a2
jiwen624 <jiwen624@gmail.com>
2022-04-25 12:44:09 -0700

    update collector endpoint

--- core/src/main/java/com/tracelytics/joboe/rpc/RpcClientManager.java
+++ core/src/main/java/com/tracelytics/joboe/rpc/RpcClientManager.java
@@ -30,7 +30,7 @@ public abstract class RpcClientManager {
     private static final Map<ClientType, RpcClientManager> registeredManagers = new HashMap<ClientType, RpcClientManager>();
     static final URL DEFAULT_COLLECTER_CERT_LOCATION = RpcClientManager.class.getResource("/collector-ca.crt"); //cert by default included in the resource folder or root folder in jar
 
-    static final String DEFAULT_HOST = "apm-collector.dc-01.cloud.solarwinds.com"; //default collector host: NH production
+    static final String DEFAULT_HOST = "apm.collector.cloud.solarwinds.com"; //default collector host: NH production
     static final int DEFAULT_PORT = 443; //default collector port
 
     protected static String collectorHost;
