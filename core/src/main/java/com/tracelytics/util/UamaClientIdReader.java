a074c1770ad7948eb178fc0778e5ddeb5a9ac212
jiwen624 <jiwen624@gmail.com>
2022-06-14 11:22:43 -0700

    remove periodic uams id checker

--- core/src/main/java/com/tracelytics/util/UamaClientIdReader.java
+++ core/src/main/java/com/tracelytics/util/UamaClientIdReader.java
@@ -19,7 +19,6 @@ import java.util.concurrent.atomic.AtomicReference;
 public class UamaClientIdReader {
     private static final String UAMS_CLIENT_ID_PATH_LINUX = "/var/solarwinds/uamsclient/uamsclientid";
     private static final String UAMS_CLIENT_ID_PATH_WIN = "C:\\ProgramData\\Solarwinds\\uamsclient\\uamsclientid";
-    private static final int UAMS_CLIENT_ID_CHECK_INTERVAL = 60;
     private static final Logger logger = LoggerFactory.getLogger();
     private static final HostInfoUtils.OsType osType = HostInfoUtils.getOsType();
     private static final String uamsClientIdFile = osType == HostInfoUtils.OsType.WINDOWS ?
@@ -27,30 +26,8 @@ public class UamaClientIdReader {
     private static final AtomicReference<String> uamsClientId = new AtomicReference<>();
     private static final AtomicReference<FileTime> lastModified = new AtomicReference<>(FileTime.from(Instant.EPOCH));
 
-    private static final AtomicBoolean uamsClientIDInitialized = new AtomicBoolean(false);
     public static String getUamsClientId() {
-        if (!uamsClientIDInitialized.get()) {
-            uamsClientIDInitialized.set(true);
-            refreshUamsClientId();
-            startUamsClientIdChecker();
-        }
-        return uamsClientId.get();
-    }
-
-    private static void startUamsClientIdChecker() {
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1, DaemonThreadFactory.newInstance("uams-client-id-checker"));
-        //align to minute, figure out the delay in ms
-        long delay = 60 * 1000 - System.currentTimeMillis() % (60 * 1000);
-        executorService.scheduleAtFixedRate(new Runnable() {
-            public void run() {
-                refreshUamsClientId();
-            }
-        }, delay, UAMS_CLIENT_ID_CHECK_INTERVAL * 1000, TimeUnit.MILLISECONDS);
-    }
-
-    private static void refreshUamsClientId() {
         try {
-            logger.debug("Refreshing uamsclientid.");
             FileTime modifiedTime = Files.getLastModifiedTime(Paths.get(uamsClientIdFile));
             if (!lastModified.get().equals(modifiedTime)) {
                 lastModified.set(modifiedTime);
@@ -60,6 +37,7 @@ public class UamaClientIdReader {
         } catch (IOException e) {
             logger.debug("I cannot read the file " + uamsClientIdFile + ", maybe it doesn't exist.");
         }
+        return uamsClientId.get();
     }
 
     private static String readFirstLine(String filePath) throws IOException {
