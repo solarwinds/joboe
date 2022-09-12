77350bd64a14e7af3e3eec4a33d8af1ae97e4463
jiwen624 <jiwen624@gmail.com>
2022-09-12 16:45:27 -0700

    set the min supported version

--- core/src/main/java/com/tracelytics/util/JavaRuntimeVersionChecker.java
+++ core/src/main/java/com/tracelytics/util/JavaRuntimeVersionChecker.java
@@ -1,7 +1,7 @@
 package com.tracelytics.util;
 
 public class JavaRuntimeVersionChecker {
-    public static final String minVersionSupported = "1.8_252"; // TODO
+    public static final String minVersionSupported = "1.8.0_252";
     public static boolean isJdkVersionSupported() {
         return JavaVersionComparator.compare(minVersionSupported, System.getProperty("java.version")) <= 0;
     }
