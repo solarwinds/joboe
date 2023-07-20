dd88d031b2cbdc353401357f9d6ded6b88edce75
cleverchuk <chubiyke@gmail.com>
2023-07-20 09:24:56 -0400

    NH-31132: Instrumentation Support for GuideWire & 1st 100K Customer -[NH-31132](https://swicloud.atlassian.net/browse/NH-39116) - add `AGENT_ATTR_RENAME` config property

--- core/src/main/java/com/tracelytics/joboe/config/ConfigProperty.java
+++ core/src/main/java/com/tracelytics/joboe/config/ConfigProperty.java
@@ -16,6 +16,7 @@ import java.util.Map;
  */
 public enum ConfigProperty {
     AGENT_CONFIG (new ConfigKey(null, EnvPrefix.PRODUCT + "CONFIG_FILE", "config"), ConfigGroup.AGENT, String.class),
+    AGENT_ATTR_RENAME (new ConfigKey("agent.attrRename", EnvPrefix.PRODUCT + "ATTR_RENAME"), ConfigGroup.AGENT, String.class),
     AGENT_DEBUG (new ConfigKey(null, null, "debug"), ConfigGroup.AGENT, Boolean.class),
     AGENT_LOGGING (new ConfigKey("agent.logging", EnvPrefix.PRODUCT + "DEBUG_LEVEL", "logging"), ConfigGroup.AGENT, String.class),
     AGENT_LOGGING_TRACE_ID (new ConfigKey("agent.logging.traceId"), ConfigGroup.AGENT, String.class),
